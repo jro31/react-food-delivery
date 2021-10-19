1297bc15c5aca96c23318f515d3a9b1811d6e70f
Jethro Williams <jethrowilliams@outlook.com>
2021-10-19 11:41:24 +0100

    Add Cart context to CartCounter and use to display number of items in cart in Navbar

--- src/components/navbar/CartCounter.js
+++ src/components/navbar/CartCounter.js
@@ -1,8 +1,14 @@
+import { useContext } from 'react';
 import styles from './CartCounter.module.css';
+import Cart from '../../store/cart';
 
 const CartCounter = () => {
+  const ctx = useContext(Cart);
+
+  const numberOfCartItems = () => ctx.cartItems.map(cartItem => cartItem.quantity).reduce((a, b) => a + b, 0);
+
   return (
-    <div className={styles['cart-counter']}>0</div>
+    <div className={styles['cart-counter']}>{numberOfCartItems()}</div>
   )
 };
 
